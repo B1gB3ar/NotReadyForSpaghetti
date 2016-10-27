fff0623d2b2dabb28f2225bff1e6dc6773917450
Dylan Fillmore <dylanfillmore7@gmail.com>
2016-10-27 19:36:42 -0400

    Added some scripts and script modifications to prepare for player movement and firing.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,23 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet{
-    public int damage;
-    public int range;
-    public double penetration;
-    public int bulletSpeed;
+public class Bullet : MonoBehaviour {
 
-    public bool affectedByGravity;
-    public bool ricochet;
+    public int dam;
+    public bool isPlayer;
+    public float bulletTime = 5.0f;
+    float currBulletTime;
 
-    public Bullet(int dam, int ran, double pen, int spd, bool grav, bool ric)
-    {
-        damage = dam;
-        range = ran;
-        penetration = pen;
-        bulletSpeed = spd;
-        affectedByGravity = grav;
-        ricochet = ric;
+    //public bool affectedByGrav;
+    //public bool rico;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currBulletTime += Time.deltaTime;
+        if (currBulletTime >= bulletTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
+    public void initBull(int gunIdent, bool playerBull)
+    {
+        //switch (gunIdent){
+            //default:
+                dam = 10;
+                bulletTime = 10;
+
+                //affectedByGrav = false;
+                //rico = false;
+               // break;
+        //}
+        isPlayer = playerBull;
+    }
+
+    void OnCollisionEnter(Collision colliInfo)
+    {
+        if (colliInfo.gameObject.tag == "player")
+        {
+            //call damage on player
+            Destroy(gameObject);
+        }
+        else if (colliInfo.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+            if (isPlayer)
+            {
+                //call damage on enemy
+            }
+        }
+        else if (colliInfo.gameObject.tag == "bullet")
+        {
+            //ignore?
+        }
+        else if (colliInfo.gameObject.tag == "pack")
+        {
+            //ignore?
+        }
+        else//terrain
+        {
+            Destroy(gameObject);
+            //TODO
+            //check penetration and recoil
+        }
+    }
 }
---
+++ Assets/Scripts/ShootGun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootGun : MonoBehaviour {
+
+    public int ammoCost;
+    public int bulletForce;
+    public int gunIdent;
+    public GameObject shooterObj;
+    //public BaseGun shooterGun;
+    public GameObject bulletObj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void shoot()
+    {
+        GameObject newObj = Instantiate(bulletObj,
+            transform.position + shooterObj.transform.forward * 2,
+            transform.rotation) as GameObject;
+        newObj.GetComponent<Rigidbody2D>().AddForce(shooterObj.transform.forward * //shooterGun.bulletSpeed);
+            5000.0f);
+        newObj.GetComponent<Bullet>().initBull(gunIdent, true);
+    }
+
+}
